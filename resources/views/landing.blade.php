02988d774d607b28e4af1a3ac54fe83463349fce
ismailbentabett <ismailbentabett@gmail.com>
2023-07-26 20:34:44 +0100

    translation

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -40,9 +40,10 @@
                                             <a href="{{ 'login' }}"
                                                 class="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
                                                 <span
-                                                    class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-bittersweet-500 to-bittersweet-600 rounded-full">We're
-                                                    Contestant ?</span>
-                                                <span class="ml-4 text-sm">Sign In</span>
+                                                    class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-bittersweet-500 to-bittersweet-600 rounded-full">
+                                                    Nous sommes Candidat ?
+                                                </span>
+                                                <span class="ml-4 text-sm">Connexion</span>
                                                 <!-- Heroicon name: solid/chevron-right -->
                                                 <svg class="ml-2 w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg"
                                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
